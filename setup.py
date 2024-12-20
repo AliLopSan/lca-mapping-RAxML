ea4c758bb3b2c0e8620f8a933305498e24384d37
Alitzel López Sánchez <lopez.alitzel@gmail.com>
2024-12-20 12:09:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lca-map",
-    version="0.0.0",
+    name="lcamap",
+    version="0.1.0",
     description="A package to build LCA maps for RAxML gene trees.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Alitzel Lopez Sanchez",
     author_email="alitzel.lopez.sanchez@usherbrooke.com",
     url="https://github.com/AliLopSan/lca-mapping-RAxML",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"":"src"}
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT license",
+    "Operating System :: OS Independent",]
     install_requires=[
-        "tralda","asymmetree"  # Example dependency for Newick parsing
+        "tralda","asymmetree","pandas"
     ],
     python_requires=">=3.7",
 )
